d366be0bb3d50620b891c950da39b0452ff9345c
dave <d@h.c>
2019-01-24 20:40:59 +0000

    displays table with menu

--- webgui/src/index.js
+++ webgui/src/index.js
@@ -1,75 +1,104 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './style.css';
+import {log,getData,getUnique,getWhere,colIndex} from './utils.js';
 import * as serviceWorker from './serviceWorker';
 
-var testdata = require('./schema.json');
-var offline = true;
+var schemaData = getData();
+var tableNames =  getUnique(schemaData[0],"TABLE_NAME");
+var tableData = getWhere(schemaData[0],"TABLE_NAME",tableNames[0]);
 
-function colIndex(queryResults,column){
-    for (var i in queryResults.Colnames)
-        if (queryResults.Colnames[i] === column)
-            return i;
-    return -1;
-}
-function getUnique(queryResults,column){
-    var uniqueList = [];
-    var idx = colIndex(queryResults,column);
-    if (idx > -1)
-        for (var i in queryResults.Vals)
-            if (uniqueList.indexOf(queryResults.Vals[i][idx]) < 0)
-                uniqueList.push(queryResults.Vals[i][idx]);
-    return uniqueList;
-}
-function getWhere(queryResults,column,value){
-    var subset = JSON.parse(JSON.stringify(queryResults));
-    var idx = colIndex(queryResults,column);
-    if (idx > -1){
-        var ri = queryResults.Vals.length - 1;
-        for (var i in queryResults.Vals){
-            if (queryResults.Vals[ri-i][idx] !== value){
-                subset.Vals.splice(ri-i,1);
-                subset.Numrows--;
-            }
-        }
-        return subset;
-    } else return null;
-}
+class TableList extends React.Component {
+    tableBut(name,idx){
+        return (
+            <div className="tableButton1" key={idx} onClick={()=>{ this.props.changeTable(name) }}>
+                {name}
+            </div>
+        )
+    }
 
-class Hmenu extends React.Component {
-    //schema = getUnique(props.s.schema,"TABLE_NAME");
     render(){
         return (
-            <div>menu goes here</div>
+            <div className="dropmenu">
+                <div className="dropButton">
+                Table Names
+                </div>
+                <div className="dropmenu-content">
+                    {tableNames.map((name,i)=>{return this.tableBut(name,i)})}
+                </div>
+            </div>
+        )
+    }
+}
+
+class QueryRender extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            hideColumns : this.props.hide
+        }
+    }
+    hideColumn(column,numeric){
+        if (!numeric)
+            column = colIndex(this.props.table,column)|0;
+        if (this.state.hideColumns.indexOf(column)<0){
+            this.state.hideColumns.push(column);
+            this.forceUpdate();
+            console.log(this.state.hideColumns);
+            console.log(column);
+        }
+    }
+    cell(name,idx,type){
+        if (this.state.hideColumns.indexOf(idx)<0)
+            if (type === 'head')
+                return( <th key={idx} className="tableCell" onClick={()=>{this.hideColumn(idx,true)}}> {name} </th>)
+            else
+                return( <td key={idx} className="tableCell"> {name} </td>)
+    }
+    row(row,type){
+        return( <tr className="tableRow"> {row.map((name,idx)=>{ return this.cell(name,idx,type); })} </tr> )
+    }
+    rows(rows){
+        return( rows.map((row,i)=>{return this.row(row)}) )
+    }
+    render(){
+        return ( 
+            <div> 
+            <TableList
+                changeTable = {(tableName)=>{this.props.changeTable(tableName)}}
+            />
+            <table className="table">
+                    <tbody>
+                    {this.row(this.props.table.Colnames,'head')}
+                    {this.rows(this.props.table.Vals)}
+                    </tbody>
+                  </table>
+            </div>
         )
     }
 }
 
 class Main extends React.Component {
-    getData(){
-        if (!offline)
-            fetch('/query')
-                .then(function(response) {
-                    return response.json();
-                })
-                .then(function(myJson) {
-                    return myJson;
-                });
-        else {
-            return testdata
+    constructor(props){
+        super(props);
+        this.state = {
+            metaTable : tableData,
         }
     }
-
+    changeTable(tableKey,tableName){
+        this.setState({[tableKey] : getWhere(schemaData[0],"TABLE_NAME",tableName) });
+    }
 
     render(){
-        var data = this.getData();
-        //console.log(data);
-        console.log(getUnique(data[0],"TABLE_NAME"));
-        console.log(getWhere(data[0],"TABLE_NAME","Customer"));
         return (
             <>
-            <h1>hello world</h1>
-            <a onClick={()=>{this.getData()}}>try query</a>
+            <QueryRender 
+                s = {this.state}
+                changeTable = {(name)=>{this.changeTable('metaTable',name)}}
+                table = {this.state.metaTable}
+                hide = {[0,1,2]}
+            />
             </>
         )
     }
