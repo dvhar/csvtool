94758606184305d4a36905062f05a8a45156d96f
dave <ydrah32@gmail.com>
2019-01-29 14:45:51 -0600

    new premade querty with constraint types like keys

--- sql.go
+++ sql.go
@@ -138,9 +138,27 @@ func premade(request string ) (string) {
     switch request {
         case "columns_total":
             return "SELECT * FROM information_schema.Columns;"
+
         case "columns_abridged":
             return `SELECT table_name, column_name, ordinal_position, data_type, is_nullable
                     FROM information_schema.columns;`
+
+        case "columns_withkey":
+            return `SELECT c.table_name, c.column_name, c.DATA_TYPE, c.IS_NULLABLE, 
+                        k.constraint_type, k.constraint_name
+                    FROM information_schema.columns as c 
+                    left join
+                    (
+                        select col.column_name, tab.table_name, tab.constraint_type, tab.constraint_name
+                        FROM   information_schema.constraint_column_usage as col
+                        join information_schema.table_constraints as tab
+                        on col.constraint_name = tab.constraint_name
+                        where tab.table_name = col.table_name
+                    )
+                    as k
+                    on c.column_name = k.column_name
+                    and c.table_name = k.table_name;`
+
         case "primaries":
             return `SELECT col.column_name, tab.table_name, tab.constraint_type, col.constraint_name
                     FROM   information_schema.constraint_column_usage as col
